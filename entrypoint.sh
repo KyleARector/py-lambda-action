b4c92361cae1359d26f42b56c146d172db0b209d
Kyle Rector <kylearector@outlook.com>
2020-04-22 10:15:06 -0400

    Hiding update-function-configuration output

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ publish_dependencies_as_layer(){
 publish_function_code(){
 	echo "Deploying the code itself..."
 	zip -r code.zip . -x \*.git\*
-	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
+	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip &> /dev/null
 }
 
 update_function_layers(){
